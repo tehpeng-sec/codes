aa314f55ef71f5a02ccead9b19931ffa9bee063a
tehpeng-sec <89267341+tehpeng-sec@users.noreply.github.com>
2022-10-29 15:00:33 +0800

    Add files via upload

---
+++ python/myFile.py
@@ -0,0 +1,7 @@
+from pathlib import Path
+
+myFiles = ['accounts.txt', 'details.csv', 'invite.docx']
+
+for filename in myFiles:
+    print(Path(r'C:\Users\Al', filename))
+
---
+++ python/workWithExl.py
@@ -0,0 +1,4 @@
+import openpyxl
+
+wb = openpyxl.load_workbook('D:\Users\swamps\Downloads\jkl.xlsx')
+
