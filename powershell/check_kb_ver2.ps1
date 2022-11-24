762ffc44dc9bffc609f8c907aba8498ad7cf3582
tehpeng <89267341+tehpeng-sec@users.noreply.github.com>
2022-11-24 15:58:32 +0800

    Create check_kb_ver2.ps1

---
+++ powershell/check_kb_ver2.ps1
@@ -0,0 +1,7 @@
+﻿$newdir = New-Item -ItemType Directory -Path ".\$((Get-Date).ToString('dd-MM-yyyy'))"
+
+$A = Get-Content -Path C:\checkkb\Servers.txt
+
+$A | ForEach-Object {
+    Invoke-Command -ComputerName $_ -ScriptBlock{Get-HotFix | where {$_.InstalledOn -gt (get-date).AddDays(-30) } | sort InstalledOn} > $newdir\$_.txt
+}
