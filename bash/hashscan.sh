5d58a5aa26a8d994760085032892c07f30632c0a
tehpeng <89267341+tehpeng-sec@users.noreply.github.com>
2022-11-30 09:41:44 +0800

    Update

--- bash/hashscan.sh
+++ bash/hashscan.sh
@@ -43,7 +43,7 @@ echo "Processed $filecount files"
 
 echo "Comparing hashes..."
 #remove blank line in hash.txt as it will affect the grep
-grep -w --color=always --file hashlinux.txt $FILENAME > $RESULT
+grep -w --color=always --file hash.txt $FILENAME > $RESULT
 echo "Completed"
 ls -lh "$RESULT"
 cat "$RESULT"
