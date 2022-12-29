ac256a9d879656118ef6d5e958aac6f9762c93c8
tehpeng <89267341+tehpeng-sec@users.noreply.github.com>
2022-12-29 09:57:44 +0800

    Delete FYP
    Deleted FYP folder

--- FYP1/dbFunctions.php
+++
@@ -1,8 +0,0 @@
-<?php
-$db_host = "aa13eprjmd79acj.cdpkcir7et7n.us-east-1.rds.amazonaws.com";
-$db_username = "admin";
-$db_password = "vampire-1";
-$db_name = "onerp";
-$link = mysqli_connect($db_host,$db_username,$db_password,$db_name) or 
-        die(mysqli_connect_error());
-?>
