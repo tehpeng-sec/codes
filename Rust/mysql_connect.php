76f06156f11ad85591138a0f40dfdf983fd1587e
tehpeng <89267341+tehpeng-sec@users.noreply.github.com>
2023-02-08 10:02:18 +0800

    Create mysql_connect.php

---
+++ Rust/mysql_connect.php
@@ -0,0 +1,15 @@
+<?php
+$servername = "localhost";
+$username = "admin";
+$password = "HelloWorld";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password);
+
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
+# this is a test code. The credendials are not real
+?>
