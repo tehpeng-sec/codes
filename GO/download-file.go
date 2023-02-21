ea8f0f0ae5759426856bb016a25f347f8b678be5
tehpeng <89267341+tehpeng-sec@users.noreply.github.com>
2023-02-21 22:23:18 +0800

    download-file.go update
    added a download URL to the code

--- GO/download-file.go
+++ GO/download-file.go
@@ -1,55 +1,55 @@
 package main
- 
+
 import (
-    "fmt"
-    "io"
-    "log"
-    "net/http"
-    "net/url"
-    "os"
-    "strings"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"net/url"
+	"os"
+	"strings"
 )
- 
+
 var (
-    fileName    string
-    fullURLFile string
+	fileName    string
+	fullURLFile string
 )
- 
+
 func main() {
- 
-    fullURLFile = "put_your_url_here"
- 
-    // Build fileName from fullPath
-    fileURL, err := url.Parse(fullURLFile)
-    if err != nil {
-        log.Fatal(err)
-    }
-    path := fileURL.Path
-    segments := strings.Split(path, "/")
-    fileName = segments[len(segments)-1]
- 
-    // Create blank file
-    file, err := os.Create(fileName)
-    if err != nil {
-        log.Fatal(err)
-    }
-    client := http.Client{
-        CheckRedirect: func(r *http.Request, via []*http.Request) error {
-            r.URL.Opaque = r.URL.Path
-            return nil
-        },
-    }
-    // Put content on file
-    resp, err := client.Get(fullURLFile)
-    if err != nil {
-        log.Fatal(err)
-    }
-    defer resp.Body.Close()
- 
-    size, err := io.Copy(file, resp.Body)
- 
-    defer file.Close()
- 
-    fmt.Printf("Downloaded a file %s with size %d", fileName, size)
- 
+
+	fullURLFile = "http://www.golang-book.com/public/pdf/gobook.pdf"
+
+	// Build fileName from fullPath
+	fileURL, err := url.Parse(fullURLFile)
+	if err != nil {
+		log.Fatal(err)
+	}
+	path := fileURL.Path
+	segments := strings.Split(path, "/")
+	fileName = segments[len(segments)-1]
+
+	// Create blank file
+	file, err := os.Create(fileName)
+	if err != nil {
+		log.Fatal(err)
+	}
+	client := http.Client{
+		CheckRedirect: func(r *http.Request, via []*http.Request) error {
+			r.URL.Opaque = r.URL.Path
+			return nil
+		},
+	}
+	// Put content on file
+	resp, err := client.Get(fullURLFile)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer resp.Body.Close()
+
+	size, err := io.Copy(file, resp.Body)
+
+	defer file.Close()
+
+	fmt.Printf("Downloaded a file %s with size %d", fileName, size)
+
 }
