849a681c96edbb646e29b3dac9afe53f0a5b2010
tehpeng <89267341+tehpeng-sec@users.noreply.github.com>
2023-10-31 18:25:40 +0800

    Update batch

---
+++ batch/install-hf.bat
@@ -0,0 +1,31 @@
+@echo off
+setlocal
+
+:: Define the path to the Splunk Heavy Forwarder installer
+set SPLUNK_INSTALLER=splunkforwarder-<version>-<platform>.msi
+
+:: Define the installation directory
+set INSTALL_DIR=C:\Program Files\SplunkUniversalForwarder
+
+:: Define the admin password (change to your desired password)
+set ADMIN_PASSWORD=your_password
+
+:: Install Splunk Heavy Forwarder
+msiexec /i %SPLUNK_INSTALLER% AGREETOLICENSE=Yes INSTALLDIR="%INSTALL_DIR%" /quiet
+
+:: Start Splunk and accept the license
+"%INSTALL_DIR%\bin\splunk" start --accept-license
+
+:: Set Splunk to start on boot
+"%INSTALL_DIR%\bin\splunk" enable boot-start
+
+:: Change the admin password
+"%INSTALL_DIR%\bin\splunk" edit user admin -password %ADMIN_PASSWORD%
+
+:: Restart Splunk
+"%INSTALL_DIR%\bin\splunk" restart
+
+:: Display a message
+echo Splunk Heavy Forwarder installation is complete.
+
+endlocal
